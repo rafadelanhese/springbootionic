4057482a9fee5accfa35b4ef31027a24c92c8e9f
Rafael Delanhese <rdmmail-1@yahoo.com.br>
2019-01-24 19:24:51 -0200

    Projeto criado

---
+++ src/main/java/com/delanhese/cursospring/SpringbootionicApplication.java
@@ -0,0 +1,14 @@
+package com.delanhese.cursospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootionicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootionicApplication.class, args);
+	}
+
+}
+
