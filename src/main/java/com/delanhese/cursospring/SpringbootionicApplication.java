39f1372771f3d29716e3ffb42553c87626e7e0e3
Rafael Delanhese <rdmmail-1@yahoo.com.br>
2019-02-01 19:23:11 -0200

    Criando operação de instanciação

--- src/main/java/com/delanhese/cursospring/SpringbootionicApplication.java
+++ src/main/java/com/delanhese/cursospring/SpringbootionicApplication.java
@@ -1,13 +1,32 @@
 package com.delanhese.cursospring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringbootionicApplication {
+import com.delanhese.cursospring.domain.Categoria;
+import com.delanhese.cursospring.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class SpringbootionicApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository; 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootionicApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
--- src/main/java/com/delanhese/cursospring/resources/CategoriaResource.java
+++ src/main/java/com/delanhese/cursospring/resources/CategoriaResource.java
@@ -1,6 +1,5 @@
 package com.delanhese.cursospring.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
