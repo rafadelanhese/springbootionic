1324a9f7877641266cc940a4dbb0d99e5bffbc41
Rafael Delanhese <rdmmail-1@yahoo.com.br>
2019-02-06 18:21:58 -0200

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/delanhese/cursospring/SpringbootionicApplication.java
+++ src/main/java/com/delanhese/cursospring/SpringbootionicApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.delanhese.cursospring.domain.Categoria;
 import com.delanhese.cursospring.domain.Cidade;
+import com.delanhese.cursospring.domain.Cliente;
+import com.delanhese.cursospring.domain.Endereco;
 import com.delanhese.cursospring.domain.Estado;
 import com.delanhese.cursospring.domain.Produto;
+import com.delanhese.cursospring.domain.enums.TipoCliente;
 import com.delanhese.cursospring.repositories.CategoriaRepository;
 import com.delanhese.cursospring.repositories.CidadeRepository;
+import com.delanhese.cursospring.repositories.ClienteRepository;
+import com.delanhese.cursospring.repositories.EnderecoRepository;
 import com.delanhese.cursospring.repositories.EstadoRepository;
 import com.delanhese.cursospring.repositories.ProdutoRepository;
 
@@ -23,18 +28,24 @@ public class SpringbootionicApplication implements CommandLineRunner{
 	private ProdutoRepository produtoRepository;	
 	private EstadoRepository estadoRepository;
 	private CidadeRepository cidadeRepository;
+	private ClienteRepository clienteRepository;
+	private EnderecoRepository enderecoRepository;
 	
 	
-	@Autowired
+	@Autowired	
 	public SpringbootionicApplication(CategoriaRepository categoriaRepository, ProdutoRepository produtoRepository,
-			EstadoRepository estadoRepository, CidadeRepository cidadeRepository) {
+			EstadoRepository estadoRepository, CidadeRepository cidadeRepository, ClienteRepository clienteRepository,
+			EnderecoRepository enderecoRepository) {
 		super();
 		this.categoriaRepository = categoriaRepository;
 		this.produtoRepository = produtoRepository;
 		this.estadoRepository = estadoRepository;
 		this.cidadeRepository = cidadeRepository;
+		this.clienteRepository = clienteRepository;
+		this.enderecoRepository = enderecoRepository;
 	}
-		
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootionicApplication.class, args);
 	}
@@ -71,6 +82,17 @@ public class SpringbootionicApplication implements CommandLineRunner{
 		
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("37363323", "93838939"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		
 	}
 
