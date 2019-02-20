2ccee6bfcd941118cf8d9ef8922189cfcd6920c5
Rafael Delanhese <rdmmail-1@yahoo.com.br>
2019-02-20 19:37:51 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/delanhese/cursospring/resources/CategoriaResource.java
+++ src/main/java/com/delanhese/cursospring/resources/CategoriaResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -61,6 +63,13 @@ public class CategoriaResource {
 	public ResponseEntity<List<CategoriaDTO>> buscarTodos() {			
 		List<Categoria> listaCategoria = categoriaService.buscarTodos();
 		List<CategoriaDTO> listaCategoriaDTO = listaCategoria.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listaCategoriaDTO);		
+	}	
+	
+	@GetMapping("/pagina")
+	public ResponseEntity<Page<CategoriaDTO>> buscarPagina(@RequestParam(value="pagina", defaultValue="0")Integer pagina, @RequestParam(value="linhasPorPagina", defaultValue="24")Integer linhasPorPagina, @RequestParam(value="ordenarPor", defaultValue="nome")String ordenarPor, @RequestParam(value="direcao", defaultValue="ASC")String direcao) {			
+		Page<Categoria> listaCategoria = categoriaService.buscarPagina(pagina, linhasPorPagina, ordenarPor, direcao);
+		Page<CategoriaDTO> listaCategoriaDTO = listaCategoria.map(obj -> new CategoriaDTO(obj));
 		return ResponseEntity.ok().body(listaCategoriaDTO);		
 	}	
 }
--- src/main/java/com/delanhese/cursospring/services/CategoriaService.java
+++ src/main/java/com/delanhese/cursospring/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.delanhese.cursospring.domain.Categoria;
@@ -46,5 +49,10 @@ public class CategoriaService {
 	
 	public List<Categoria> buscarTodos(){
 		return categoriaRepository.findAll();
+	}
+	
+	public Page<Categoria> buscarPagina(Integer pagina, Integer linhasPorPagina, String ordenarPor, String direcao){
+		PageRequest pageRequest = PageRequest.of(pagina,  linhasPorPagina, Direction.valueOf(direcao), ordenarPor);
+		return categoriaRepository.findAll(pageRequest);
 	}
 }
